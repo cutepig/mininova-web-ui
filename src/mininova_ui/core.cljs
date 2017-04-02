5c26d626bab0216b477c0d43be65e54d9394bd4d
Alex Seidler <alex.seidler@futurice.com>
2017-04-02 15:27:26 +0300

    Initial commit

---
+++ src/mininova_ui/core.cljs
@@ -0,0 +1,20 @@
+(ns mininova-ui.core
+  (:require [reagent.core :as reagent :refer [atom]]
+            [mininova-ui.ui :as ui]))
+
+(enable-console-print!)
+
+(println "This text is printed from src/mininova-ui/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(reagent/render-component [ui/main-panel]
+                          (. js/document (getElementById "app")))
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
---
+++ src/mininova_ui/ui.cljs
@@ -0,0 +1,41 @@
+(ns mininova-ui.ui
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as rf]))
+
+
+(rf/reg-event-db ::panel
+  [rf/debug]
+  (fn [db [_ panel]]
+    (assoc db ::panel panel)))
+
+(rf/reg-sub ::panel
+  (fn [db _]
+    (::panel db)))
+
+(defn tabs []
+  (let [tab (rf/subscribe [::panel])]
+    [:ul.tabs
+      [:li {:class (if (= tab :osc) "is-active")
+            :on-click #(rf/dispatch [::panel :osc])}
+        "Osc"]
+      [:li {:class (if (= tab :filter) "is-active")
+            :on-click #(rf/dispatch [::panel :filter])}
+        "Filter"]]))
+
+(defn osc-panel []
+  [:div.osc-panel
+    [:h2 "Oscillator"]])
+
+(defn filter-panel []
+  [:div.filter-panel
+    [:h2 "Filter"]])
+
+(defn main-panel []
+  (let [tab @(rf/subscribe [::panel])]
+    [:main.main-panel
+      [tabs]
+      (condp = tab
+        :osc [osc-panel]
+        :filter [filter-panel]
+        [:h2 "Select a panel"])]))
+  
